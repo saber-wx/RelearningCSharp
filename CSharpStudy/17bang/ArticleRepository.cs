913d1ae06595fdfdabf311b8ef60d8e55c923c34
文轩 <saber.wx@qq.com>
2019-06-25 09:43:42 +0800

    建模

---
+++ CSharpStudy/17bang/ArticleRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpStudy._17bang
+{
+    class ArticleRepository : IArticleRepository
+    {
+        private static IList<Article> Trepository;
+        IList<Article> Get()
+        {
+            return Trepository;
+        }
+        void Add(Article article)
+        {
+            if (Trepository == null)
+            {
+                Trepository = new List<Article>();
+            }
+            else
+            {
+                Trepository.Add(article);
+            }
+        }
+        
+        Article GetByAuthor(string Author)
+        {
+            return null;
+        }
+
+        List<Article> IRepository<Article>.Get()
+        {
+            return null;
+        }
+    }
+}
---
+++ CSharpStudy/17bang/IRepertory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpStudy._17bang
+{
+
+    interface IRepository<T>
+    {
+        List<T> Get();
+
+    }
+    interface IArticleRepository : IRepository<Article>
+    {
+
+    }
+}

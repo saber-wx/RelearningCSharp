4446d10e8a3dbb9425d05c84e3cbdfa15d0929eb
文轩 <saber.wx@qq.com>
2019-07-18 00:14:50 +0800

    添加数据库未完成

--- BLL/Repository/UserRepository.cs
+++ BLL/Repository/UserRepository.cs
@@ -20,5 +20,14 @@ namespace BLL.Repository
         {
             return _users.Where(u => u.Name == userName).SingleOrDefault();
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=17Help;Integrated Security=True;";
+            optionsBuilder.UseSqlServer(connectionString);
+
+
+        }
+
     }
 }

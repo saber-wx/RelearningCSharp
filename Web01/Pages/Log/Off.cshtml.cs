ad9e3c0bc705f4f6913c5d422cae7e2f59816b3a
文轩 <saber.wx@qq.com>
2019-07-23 11:24:56 +0800

    重新整理Log/Off

--- Web01/Pages/Log/Off.cshtml.cs
+++ Web01/Pages/Log/Off.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web01.Pages.Shared;
 
 namespace Web01.Pages.Log
 {
-    public class OffModel : PageModel
+    public class OffModel : _LayoutModel
     {
-        public void OnGet()
+        public OffModel()
         {
 
+        }
+        public override void OnGet()
+        {
+            if (Request.Cookies["userId"] != null && Request.Cookies["userAuth"] != null)
+            {
+                Response.Cookies.Delete("userId");
+                Response.Cookies.Delete("userAuth");
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("/index");
         }
     }
 }

31066dca61d7ee0f2e5b2f0759e888bdc94f36a9
文轩 <saber.wx@qq.com>
2019-07-26 23:06:03 +0800

    创建IRegisterService接口

---
+++ SRV/IRegisterService.cs
@@ -0,0 +1,28 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SRV
+{
+    public interface IRegisterService
+    {
+        User Register(string userName, string password);
+
+        UserModel GetById(int id);
+
+        //比较用户输入的密码和数据存储的密码
+        bool PasswordCorrect(string rawPassword, string MD5Password);
+
+        UserModel MapFrom(User user);
+
+        bool HasExist(string userName);
+        
+        bool ValidateEmail(int id, string code);
+
+
+
+
+
+    }
+}
--- SRV/RegisterService.cs
+++ SRV/RegisterService.cs
@@ -7,44 +7,106 @@ namespace SRV
     public class RegisterService
     {
         public UserRepository _userRepository;
+        public EmailRepository _emailRepository;
         public RegisterService()
         {
             _userRepository = new UserRepository();
         }
 
-        public User Register(string userName, string password)
-        {
-            User user = new User{Name = userName,Password = password};
-            user.Register();
-            _userRepository.Save(user);
-            return user;
-        }
-        
-        public UserModel GetById(int id)
-        {
-            User user = _userRepository.GetById(id);
-            return MapFrom(user);
-        }
+        //public User Register(string userName, string password)
+        //{
+        //    User user = new User{Name = userName,Password = password};
+        //    user.Register();
+        //    _userRepository.Save(user);
+        //    return user;
+        //}
 
-        private UserModel MapFrom(User user)
-        {
-            if (user == null)
-            {
-                return null;
-            }
-            else
-            {
-                UserModel model = new UserModel
-                {
-                    Id = user.Id,
-                    Name = user.Name,
-                    MD5Password = user.Password
-                };
-                return model;
-            }
-        }
+        //public UserModel GetById(int id)
+        //{
+        //    User user = _userRepository.GetById(id);
+        //    return MapFrom(user);
+        //}
 
-        public UserModel GetByName(string userName)
+        //private UserModel MapFrom(User user)
+        //{
+        //    if (user == null)
+        //    {
+        //        return null;
+        //    }
+        //    else
+        //    {
+        //        UserModel model = new UserModel
+        //        {
+        //            Id = user.Id,
+        //            Name = user.Name,
+        //            MD5Password = user.Password
+        //        };
+        //        return model;
+        //    }
+        //}
+
+        //public UserModel GetByName(string userName)
+        //{
+        //    User user = _userRepository.GetByName(userName);
+        //    return MapFrom(user);
+        //}
+
+        //public UserModel Login(string userName, string password)
+        //{
+        //    User user = _userRepository.GetByName(userName);
+        //    if (user == null)
+        //    {
+        //        return null;
+        //    }
+        //    else
+        //    {
+        //        UserModel model = new UserModel();
+        //        model.Id = user.Id;
+        //        model.MD5Password = user.Password;
+        //        return model;
+        //    }
+        //}
+
+        //public bool HasExist(string userName)
+        //{
+        //    return _userRepository.GetByName(userName) != null;
+        //}
+
+        ////比较用户输入的密码和数据存储的密码
+        //public bool PasswordCorrect(string rawPassword, string MD5Password)
+        //{
+        //    return User.GetMd5Hash(rawPassword) == MD5Password;
+        //}
+
+        ////在Service层通过Repository获得用户信息： 
+        //public UserModel GetUser( string userName)
+        //{
+        //    User user = _userRepository.GetByName(userName);
+        //    if (user == null)
+        //    {
+        //        return null;
+        //    }
+        //    else
+        //    {
+        //        UserModel model = new UserModel();
+        //        model.Id = user.Id;
+        //        model.MD5Password = user.Password;
+
+        //        return model;
+        //    }
+        //}
+
+        //public bool ValidateEmail(int id,string code)
+        //{
+        //    Email email = _emailRepository.Get(id);
+        //    email.Validate();
+        //    _userRepository.Flush();
+        //    return email.ValidationCode == code;
+        //}
+
+
+
+        UserModel GetByName(string userName)
         {
             User user = _userRepository.GetByName(userName);
             return MapFrom(user);
@@ -66,19 +128,10 @@ namespace SRV
             }
         }
 
-        public bool HasExist(string userName)
-        {
-            return _userRepository.GetByName(userName) != null;
-        }
-
-        //比较用户输入的密码和数据存储的密码
-        public bool PasswordCorrect(string rawPassword, string MD5Password)
-        {
-            return User.GetMd5Hash(rawPassword) == MD5Password;
-        }
+        
 
         //在Service层通过Repository获得用户信息： 
-        public UserModel GetUser( string userName)
+        public UserModel GetUser(string userName)
         {
             User user = _userRepository.GetByName(userName);
             if (user == null)
@@ -94,14 +147,6 @@ namespace SRV
                 return model;
             }
         }
-
-        //public bool ValidateEmail(int id,string code)
-        //{
-        //    Email email = _emailRepository.Get(id);
-        //    email.Validate();
-        //    _userRepository.Flush();
-        //    return email.ValidationCode == code;
-        //}
 
     }
 

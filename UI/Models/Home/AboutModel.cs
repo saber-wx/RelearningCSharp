faaee339d359dfbb0f74c6582666d2f416215ec2
文轩 <saber.wx@qq.com>
2019-08-03 22:45:56 +0800

    添加.NET Framework的页面

---
+++ UI/Models/Home/AboutModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Home
+{
+    public class AboutModel
+    {
+        public string Greet { get; } = "hello";
+    }
+}

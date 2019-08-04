b7781d4e4267ec1d017f54d92c4ba1492392b575
文轩 <saber.wx@qq.com>
2019-08-04 21:52:41 +0800

    ActionResult

---
+++ UI/Controllers/RegisterController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.Models.Register;
+
+namespace UI.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        public ActionResult Index(int? id,string UserName)
+        {
+
+            IndexModel model = new IndexModel
+            {
+                UserName = "Saber",
+                IsMale = true
+            };
+            ViewData["greet"] = "Hello,Hello";
+
+            ViewBag.Id = id;
+            ViewBag.userName = UserName;
+
+            //return View("Failed");//除shared和同一文件夹外，如果需要跳转需要写完整路径带.cshtml后缀
+            //return View(model);//还可以传model,可以设置默认传值
+            return PartialView(model);//还可以传model,可以设置默认传值
+        }
+
+        [HttpPost]
+        public ActionResult Index(IndexModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult Reminder(int? id)//部分
+        {
+            //User user = new User
+            //{
+            //    ID = 22,
+            //    Name = "saber"
+            //};
+
+            ViewBag.Id = id;
+            return PartialView();
+        }
+
+        //public ActionResult Index(/*string userNanme, IndexModel model*/)
+        //{
+        //    //if (!ModelState.IsValid)
+        //    //{
+        //    //    return View(model);
+        //    //}
+
+        //    //ViewBag.userName = userNanme;
+
+        //    ////return File();
+        //    return View();
+        //}
+
+
+
+    }
+}
---
+++ UI/Models/Register/IndexModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models.Register
+{
+    public class IndexModel
+    {
+
+        [Required(ErrorMessage = "用户名不能为空")]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public bool? IsMale { get; set; }
+
+        public string SelfIntroduction { get; set; }
+
+        public Cities? InCity { get; set; }
+
+        public bool RemberMe { get; set; }
+
+    }
+
+    public enum Cities
+    {
+        chongqing,
+        chengdu,
+        [Display(Name = "武汉")]
+        wuhan
+    }
+}
